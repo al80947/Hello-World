72687f73efc46b0d6bb815bae57135d7be75334e
Andrew Le <69644789+al80947@users.noreply.github.com>
2020-08-13 10:55:31 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I like to exercise at home");
+		System.out.println("My favorite video game is LOL");
+		System.out.println("I want to go to San Jose State University");
+		System.out.println("I'm considering majoring in Computer Science");
+	}   
+}
